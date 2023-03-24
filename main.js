56f13d17733bd08820a89a2c1853e95a05e5687f
TheMenta <91569408+TheMenta@users.noreply.github.com>
2023-03-24 13:47:40 +0100

    first

---
+++ main.js
@@ -0,0 +1,25 @@
+import { ADATLISTA } from "./adat.js";
+$(function () {
+  const articleElem = $("article");
+  articleElem.append(osszeAllit(ADATLISTA));
+
+});
+console.log(osszeAllit(ADATLISTA));
+function osszeAllit(lista) {
+  let txt = "";
+
+  txt += `<table class="table table-stripped ">`;
+  txt += `<tr class="table-dark"><th>név</th>
+    <th>kor</th>
+    <th>fajta</th></tr>`;
+  for (let index = 0; index < lista.length; index++) {
+    txt += `<tr>`;
+    for (const key in lista[index]) {
+      txt += `<td>${lista[index][key]}</td>`;
+    }
+    `</tr>`;
+  }
+  txt += `</table>`;
+  console.log(txt);
+  return txt;
+}
